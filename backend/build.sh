f5b223f855ee5793395b760606c42547233bbff9
Rafshan <ahmedrafshan1342@gmail.com>
2026-07-23 20:18:47 +0800

    New buil.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
-
-# Create superuser and ignore the error if it already exists
-python manage.py createsuperuser --noinput || true
